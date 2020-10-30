87500534fe14bfb034f21d7ff2162c22b3b9f8af
username <2951283197@qq.com>
2020-10-30 09:42:22 +0800

    Initial commit

---
+++ src/main/java/sunny/Zuoye1.java
@@ -0,0 +1,7 @@
+package sunny;
+
+public class Zuoye1 {
+    public static void main(String[] args) {
+        System.out.println("武汉加油！");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>武汉加油！！</h2>
+</body>
+</html>
